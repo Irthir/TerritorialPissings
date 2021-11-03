2c4b762fecb4093e370d30f3ca1520cf02f15248
Irthir <44899500+Irthir@users.noreply.github.com>
2021-11-03 14:18:04 +0100

    Premier commit

---
+++ Source/TerritorialPissings/TerritorialPissingsGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TerritorialPissingsGameMode.h"
+#include "TerritorialPissingsCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATerritorialPissingsGameMode::ATerritorialPissingsGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
